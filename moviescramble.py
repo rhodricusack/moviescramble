dc0fb53966bc4c2ccd0702fffcbd86740d768e1d
Rhodri Cusack <rhodricusack@Rhodris-iMac.local>
2020-10-14 13:55:00 +0100

    Tidied naming

--- moviescramble.py
+++ moviescramble.py
@@ -4,8 +4,14 @@ import random
 import numpy as np
 
 chunksize=2.0
-infn=['moana_saves_turtle.mp4']
-outfn='moana_saves_turtle_%fs.mp4'%chunksize
+
+# Example with just one video
+infn=['../videos/moana_saves_turtle.mp4']
+outfn='../videos/moana_saves_turtle_scrambled_%fs.mp4'%chunksize
+
+# # Example with just two videos intermixed
+# infn=['../videos/bathsong.mp4','../videos/mountains.mp4']
+# outfn='../videos/mixed_scrambled_%fs.mp4'%chunksize
 
 def load_video(infn):
     # Load metadata
@@ -15,31 +21,31 @@ def load_video(infn):
     fps=nframes/dur
     print('Video %s duration %f nrames %d fps %f'%(infn,dur,nframes,fps))
 
-    # Load all video
-    allvideo=skvideo.io.vread(infn)
-    print(allvideo.shape)
-    allvideo=allvideo.astype(np.uint8)
-    return metadata,allvideo,dur,fps
+    # Load a video
+    singlevideo=skvideo.io.vread(infn)
+    print(singlevideo.shape)
+    singlevideo=singlevideo.astype(np.uint8)
+    return metadata,singlevideo,dur,fps
     
 # Load up multiple videos in list and shuffle across them
-multi_allvideo=None
+allvideos=None
 multi_dur=0
 multi_fps=None
 for file in infn:
-    metadata, allvideo, dur, fps= load_video(file)
-    if multi_allvideo is None:
-        multi_allvideo=allvideo
+    metadata, singlevideo, dur, fps= load_video(file)
+    if allvideos is None:
+        allvideos=singlevideo
         multi_fps=fps
     else:
-        multi_allvideo=np.concatenate((multi_allvideo,allvideo),axis=0)
+        allvideos=np.concatenate((allvideos,singlevideo),axis=0)
         # if not fps==multi_fps:
         #     raise("FPS must be the same for all videos")
         
     multi_dur+=dur
 
-allvideo=multi_allvideo
+singlevideo=allvideos
 dur=multi_dur
-nframes=allvideo.shape[0]
+nframes=singlevideo.shape[0]
 
 # Shuffled order
 nchunks=math.ceil(dur/chunksize)
@@ -54,11 +60,5 @@ for chunktime in rorder:
     lowframe=round(chunktime*chunksize*fps)
     highframe=min(nframes, round((chunktime+1)*chunksize*fps))
     for frame in range(lowframe,highframe):
-        writer.writeFrame(allvideo[frame,:,:,:])
+        writer.writeFrame(singlevideo[frame,:,:,:])
 
-
-# for frame in reader.nextFrame():
-#     nframe+=1
-#     skvideo.io.vwrite(outfn, frame)
-#     if nframe % 100 ==0:
-#         print(f'Frame {nframe}')
